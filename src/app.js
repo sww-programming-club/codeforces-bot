8726d8d385595660be6ed0dc9640075d2a99164c
SWW Programmer <swwprogrammers@gmail.com>
2018-11-28 06:59:47 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
+const request = require('request')
+
 //If the environment we are running in is not a production environment 
 //then configure the development environment
 if (process.env.NODE_ENV != 'PRODUCTION') 
     require('dotenv').config()
 
 console.log('Launching Codeforces Bot...')
+
+var http = require("http");
+
+setInterval(() => {
+    request("https://codeforces-bot.herokuapp.com/", { json: true }, (err, res, body) => {
+        console.log('Ping!')
+    })
+}, 1500000);
 
 //Setup the database system and pass in a 
 //callback that initializes the bot with the db object on success
